7a3a480d503bd3313a5df859d205b1a1a6ddcdcb
imtiaz <waxxan.imtiaz.123@gmail.com>
2017-05-19 15:22:06 +0500

    connecting with server

--- app/src/main/java/com/example/wassa_000/technician/ComplainFragment.java
+++ app/src/main/java/com/example/wassa_000/technician/ComplainFragment.java
@@ -9,10 +9,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.MultiAutoCompleteTextView;
 
 
 public class ComplainFragment extends Fragment{
 
+    private MultiAutoCompleteTextView tvComplain;
+    private Button btnSubmit;
     public ComplainFragment() {
         // Required empty public constructor
     }
@@ -25,8 +29,20 @@ public class ComplainFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_complain, container, false);
+
+        tvComplain = (MultiAutoCompleteTextView)v.findViewById(R.id.complain);
+        btnSubmit = (Button)v.findViewById(R.id.btn_submit);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_complain, container, false);
+
+//        btnSubmit.setOnClickListener(new ButtonOnClickListener());
+        return v;
     }
 
+    private class ButtonOnClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+
+        }
+    }
 }
