6a931a447fb07b4e16840de898adfbb7bbd314d3
imtiaz <waxxan.imtiaz.123@gmail.com>
2017-05-30 12:09:43 +0500

    setting details

---
+++ app/src/main/java/com/example/wassa_000/technician/adapter/PymentHistoryAdapter.java
@@ -0,0 +1,35 @@
+package com.example.wassa_000.technician.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.example.wassa_000.technician.R;
+
+/**
+ * Created by Admin on 5/30/2017.
+ */
+
+public class PymentHistoryAdapter  extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        //view = inflter.inflate(R.layout.activity_gridview, null);//set layout for displaying items
+        return null;
+    }
+}
